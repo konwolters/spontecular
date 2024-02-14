3abcd4efc4b0f895b6196a13bf8a6ad51603af73
konstantinwolters <kon86@gmx.net>
2024-02-14 19:54:26 +0100

    first commit

---
+++ src/main/java/com/example/spontecular/SpontecularApplication.java
@@ -0,0 +1,13 @@
+package com.example.spontecular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpontecularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpontecularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spontecular/SpontecularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spontecular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpontecularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
