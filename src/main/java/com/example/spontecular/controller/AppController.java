27480ef9453133ecb6621c71348981a8764c3528
konstantinwolters <kon86@gmx.net>
2024-02-26 20:12:57 +0100

    Remove custom implementation of OpenAI api support and use spring ai.

--- src/main/java/com/example/spontecular/controller/AppController.java
+++ src/main/java/com/example/spontecular/controller/AppController.java
@@ -1,14 +1,35 @@
 package com.example.spontecular.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.ChatClient;
+import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Map;
 
 @Controller
+@RequiredArgsConstructor
 public class AppController {
+    private final ChatClient chatClient;
 
-     @GetMapping("/")
-     public String index() {
+    @GetMapping("/")
+    public String index() {
 
-         return "index";
-     }
+        return "index";
+    }
+
+    @GetMapping("/ai/generate")
+    public String generate(Model model, @RequestParam(value = "message", defaultValue = "Tell me about your day in a short assay in json format.") String message) {
+        SystemMessage systemMessage = new SystemMessage("You are a pirate and talk accordingly.");
+        UserMessage userMessage = new UserMessage(message);
+        Prompt prompt = new Prompt(List.of(systemMessage, userMessage));
+        model.addAttribute("gptResponse", chatClient.call(prompt).getResult().getOutput().getContent());
+        return "index";
+    }
 }
--- src/main/java/com/example/spontecular/service/GptService.java
+++ src/main/java/com/example/spontecular/service/GptService.java
@@ -1,13 +1,12 @@
 package com.example.spontecular.service;
 
-import com.example.spontecular.client.OpenAiClient;
-import com.example.spontecular.dto.GptMessage;
-import com.example.spontecular.dto.GptRequest;
-import com.example.spontecular.dto.GptResponse;
-import com.example.spontecular.dto.ResponseFormat;
 import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.ChatClient;
+import org.springframework.ai.chat.ChatResponse;
+import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
@@ -15,43 +14,19 @@ import java.util.Arrays;
 @Service
 @RequiredArgsConstructor
 public class GptService {
-    private final OpenAiClient openAiClient;
+    private final ChatClient chatClient;
 
-    @Value("${openai.api.model}")
-    private String apiModel;
-
-    ResponseFormat responseFormat = new ResponseFormat("json_object");
+    @Value("${SYSTEM.MESSAGE}")
+    private String systemMessage;
 
     public String getGptResponseMessage(String inputText, String prompt) {
-        GptMessage systemMessage= GptMessage.builder()
-                .role("system")
-                .content("")
-                .build();
+        SystemMessage systemMessage = new SystemMessage(this.systemMessage);
+        UserMessage inputMessage = new UserMessage(inputText);
+        UserMessage promptMessage = new UserMessage(prompt);
 
-        GptMessage inputMessage = GptMessage.builder()
-                .role("user")
-                .content(inputText)
-                .build();
-
-        GptMessage promptMessage = GptMessage.builder()
-                .role("user")
-                .content(prompt)
-                .build();
-
-        GptRequest gptRequest = GptRequest.builder()
-                .model(apiModel)
-                .temperature(0)
-                .response_format(responseFormat)
-                .messages(Arrays.asList(systemMessage, inputMessage, promptMessage))
-                .build();
-
-        ResponseEntity<GptResponse> apiResponse = openAiClient.getResponse(gptRequest);
-
-        return apiResponse
-                .getBody()
-                .getChoices()
-                .get(0)
-                .getMessage()
-                .getContent();
+        ChatResponse chatResponse = chatClient.call(
+                new Prompt(Arrays.asList(systemMessage, inputMessage, promptMessage))
+        );
+        return chatResponse.getResult().getOutput().getContent();
     }
 }
