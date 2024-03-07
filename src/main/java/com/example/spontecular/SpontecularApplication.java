e00b324db4ee6abbf9b325820f5bbe0a0f05ad6b
konstantinwolters <kon86@gmx.net>
2024-03-07 23:32:37 +0100

    Add tailwind as non cdn.

--- src/main/java/com/example/spontecular/SpontecularApplication.java
+++ src/main/java/com/example/spontecular/SpontecularApplication.java
@@ -3,7 +3,7 @@ package com.example.spontecular;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example.spontecular")
 public class SpontecularApplication {
 
     public static void main(String[] args) {
