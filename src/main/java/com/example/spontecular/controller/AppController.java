839b7968a2bb9379086595983993f97236f2036c
konstantinwolters <kon86@gmx.net>
2024-04-29 20:11:24 +0200

    Implement export view.

--- src/main/java/com/example/spontecular/controller/AppController.java
+++ src/main/java/com/example/spontecular/controller/AppController.java
@@ -1,6 +1,8 @@
 package com.example.spontecular.controller;
 
+import com.example.spontecular.dto.Classes;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.ChatClient;
 import org.springframework.ai.chat.messages.SystemMessage;
@@ -9,6 +11,7 @@ import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.IOException;
@@ -21,6 +24,13 @@ public class AppController {
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @PostMapping("/export")
+    public String export(Model model, HttpSession session) {
+        Classes classes = (Classes) session.getAttribute("classes");
+        model.addAttribute("content", classes.toString());
+        return "export";
     }
 
     @GetMapping("favicon.ico")
