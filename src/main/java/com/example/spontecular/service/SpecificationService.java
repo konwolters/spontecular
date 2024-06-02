bbf0f5f8610cd014e7b5f304319120263c413b62
konstantinwolters <kon86@gmx.net>
2024-06-02 11:39:33 +0200

    Refactor SpecificationService to increase testability by migrating third party library functionality to utility class TextExtractor.

--- src/main/java/com/example/spontecular/service/SpecificationService.java
+++ src/main/java/com/example/spontecular/service/SpecificationService.java
@@ -1,5 +1,6 @@
 package com.example.spontecular.service;
 
+import com.example.spontecular.service.utility.TextExtractor;
 import lombok.RequiredArgsConstructor;
 import org.apache.pdfbox.Loader;
 import org.apache.pdfbox.pdmodel.PDDocument;
@@ -17,11 +18,16 @@ import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @Service
-@RequiredArgsConstructor
 public class SpecificationService {
 
-    @Value("classpath:/specification/satellite.txt")
-    private Resource exampleSpecification;
+    private final Resource exampleSpecification;
+    private final TextExtractor textExtractor;
+
+    public SpecificationService(@Value("classpath:/specification/satellite.txt") Resource exampleSpecification,
+                                TextExtractor textExtractor) {
+        this.exampleSpecification = exampleSpecification;
+        this.textExtractor = textExtractor;
+    }
 
     public String loadExampleSpecification() {
         String specification;
@@ -35,58 +41,11 @@ public class SpecificationService {
     }
 
     public String loadPdfSpecification(MultipartFile pdfFile) {
-        String text;
-
-        try (InputStream inputStream = pdfFile.getInputStream()) {
-            byte[] pdfBytes = toByteArray(inputStream);
-            try (PDDocument document = Loader.loadPDF(pdfBytes)) {
-                PDFTextStripper stripper = new PDFTextStripper();
-                text = stripper.getText(document);
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        return text;
+        return textExtractor.extractTextFromPdf(pdfFile);
     }
 
-    private byte[] toByteArray(InputStream inputStream) throws IOException {
-        try (ByteArrayOutputStream buffer = new ByteArrayOutputStream()) {
-            int nRead;
-            byte[] data = new byte[16384];
-            while ((nRead = inputStream.read(data, 0, data.length)) != -1) {
-                buffer.write(data, 0, nRead);
-            }
-            buffer.flush();
-            return buffer.toByteArray();
-        }
-    }
 
     public String loadWordSpecification(MultipartFile wordFile) {
-        StringBuilder sb = new StringBuilder();
-
-        try (InputStream inputStream = wordFile.getInputStream()) {
-            XWPFDocument document = new XWPFDocument(inputStream);
-
-            // Extract text from paragraphs
-            List<XWPFParagraph> paragraphs = document.getParagraphs();
-            for (XWPFParagraph para : paragraphs) {
-                sb.append(para.getText()).append("\n");
-            }
-
-            // Extract text from tables
-            List<XWPFTable> tables = document.getTables();
-            for (XWPFTable table : tables) {
-                for (XWPFTableRow row : table.getRows()) {
-                    for (XWPFTableCell cell : row.getTableCells()) {
-                        sb.append(cell.getText()).append("\t");
-                    }
-                    sb.append("\n");
-                }
-            }
-        } catch (IOException e) {
-            throw new RuntimeException("Failed to read Word file", e);
-        }
-
-        return sb.toString();
+        return textExtractor.extractTextFromWord(wordFile);
     }
 }
---
+++ src/test/java/com/example/spontecular/service/SpecificationServiceTest.java
@@ -0,0 +1,6 @@
+package com.example.spontecular.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+class SpecificationServiceTest {
+  
+}
