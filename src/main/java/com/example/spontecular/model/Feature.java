b58b7a737346c8a3b8aaa71be0544f1526a5287b
konstantinwolters <kon86@gmx.net>
2024-06-10 19:44:31 +0200

    Refactor Feature ENUM and fragments.html to use less model attributes.

--- src/main/java/com/example/spontecular/model/Feature.java
+++ src/main/java/com/example/spontecular/model/Feature.java
@@ -1,30 +1,24 @@
 package com.example.spontecular.model;
 
 import java.util.Map;
-
+//TODO: Remove the map and find other ways to adjust the fragments to the specific feature type
 public enum Feature {
-    CLASSES("Classes:", "/getHierarchy", "hierarchyDiv"),
-    HIERARCHY("Hierarchy:", "/getRelations", "relationsDiv"),
-    RELATIONS("Non-taxonomic Relations:", "/getConstraints", "constraintsDiv"),
-    CONSTRAINTS("Constraints:", null, null);
+    CLASSES( "hierarchy"),
+    HIERARCHY("relations"),
+    RELATIONS( "constraints"),
+    CONSTRAINTS(null);
 
-    private final String fieldTitle;
-    private final String endpointUrl;
-    private final String targetElementId;
+    private final String nextFeature;
 
-    Feature(String fieldTitle, String endpointUrl, String targetElementId) {
-        this.fieldTitle = fieldTitle;
-        this.endpointUrl = endpointUrl;
-        this.targetElementId = targetElementId;
+    Feature(String nextFeature) {
+        this.nextFeature = nextFeature;
     }
 
     public Map<String, String> getResponseMap(String response) {
         return Map.of(
                 "gptResponseMessage", response,
                 "feature", this.name().toLowerCase(),
-                "fieldTitle", this.fieldTitle,
-                "endpointUrl", this.endpointUrl == null ? "" : this.endpointUrl,
-                "targetElementId", this.targetElementId == null ? "" : this.targetElementId
+                "nextFeature", this.nextFeature == null ? "" : this.nextFeature
         );
     }
 }
