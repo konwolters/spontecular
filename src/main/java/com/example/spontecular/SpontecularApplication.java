8140078910b3fb9ab81fd76554cc087d23b93921
konstantinwolters <kon86@gmx.net>
2024-06-15 22:44:22 +0200

    Fix issue that led to new hierarchy elements being added to the wrong html container.

--- src/main/java/com/example/spontecular/SpontecularApplication.java
+++ src/main/java/com/example/spontecular/SpontecularApplication.java
@@ -1,10 +1,5 @@
 package com.example.spontecular;
 
-import com.example.spontecular.dto.Classes;
-import com.example.spontecular.dto.HierarchyItem;
-import com.example.spontecular.dto.formDtos.ClassItem;
-import org.springframework.ai.parser.BeanOutputParser;
-import org.springframework.ai.parser.OutputParser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,13 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpontecularApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(SpontecularApplication.class, args);
-
-        OutputParser op1 = new BeanOutputParser(ClassItem.class);
-        OutputParser op2 = new BeanOutputParser(HierarchyItem.class);
-
-        System.out.println("Bean1: " + op1.getFormat());
-        System.out.println("Bean2: " + op2.getFormat());
     }
 }
